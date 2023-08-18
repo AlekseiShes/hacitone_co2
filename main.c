70706ec4cfd83c3ff4e56acd4f52b3928ad35321
Felisa Sivir <felisasi@va-d1.kzn.21-school.ru>
2023-08-18 22:38:24 +0300

    teatime

--- main.c
+++ main.c
@@ -13,6 +13,11 @@ typedef struct vectC{
 	float c;
 } vectC;
 
+typedef struct orient{
+	vectC pos;
+	vectC dir;
+} orient;
+
 typedef struct Measure{
 	unsigned count;
 	vectC point[CountMeasurePoints];
@@ -43,6 +48,7 @@ float calcConcentration(vectC * src, vectC * MeasurePoint){
 void init(vectC * RealSource, Measure * msr){
 	if (RealSource && msr){
 		setRandPos(RealSource);
+		RealSource->x += 10;
 		RealSource->c = 1;
 		for (unsigned i = 0; i < msr->count; i++){
 			setRandPos(msr->point+i);
@@ -66,12 +72,75 @@ void printVect(char* name, vectC * v){
 	if (v) printf("%10s\t\t%.7f\t%.7f\t%.7f\n", name, v->x, v->y, v->c);
 }
 
-void print_Data(vectC * RealSource, Measure * msr, vectC * Vect_MaxC){
+void print_Data(vectC * RealSource, Measure * msr, vectC * Vect_MaxC, orient *orients){
 	printf("Name\t\t\tx\t\ty\t\tc\n");
 	printVect("RealSource", RealSource);
 	for (unsigned i = 0; msr && i < msr->count; i++)
 		printVect("msr->point:", msr->point +i);
+	for (unsigned i = 0; msr && i < CountMeasurePoints-1; i++){
+		printVect("____dir", &(orients[i].dir));
+		printVect("____pos", &(orients[i].pos));
+	}
 	printVect("Vect_MaxC", Vect_MaxC);
+}
+
+void summvect(vectC * res, vectC *a, vectC *b){
+	if (res && (a || b)){
+		if (a){
+			res->x = a->x + (b ? b->x: 0);
+			res->y = a->y + (b ? b->y: 0);
+		} else {
+			summvect(res, b, a);
+		}
+	}
+}
+
+void multvectnum(vectC * res, vectC *a, float num){
+	if (res){
+		res->x = (a? a->x: res->x) * num;
+		res->y = (a? a->y: res->y)  * num;
+	}
+}
+
+void calcpos(vectC ** vecset, vectC * pos){
+	if (vecset && *vecset && pos){
+		pos->x = pos->y = 0;
+		summvect(pos, pos, vecset[0]);
+		summvect(pos, pos, vecset[1]);
+		summvect(pos, pos, vecset[2]);
+	}
+	multvectnum(pos, pos, 1.0/3.0);
+}
+
+void calcOrient(vectC ** vecset, orient*orients){
+	if (vecset && *vecset && orients){
+		calcpos(vecset, &(orients->pos));
+	}
+}
+
+void OrientInit(orient*orients, Measure * msr, vectC * Vect_MaxC){
+	if (orients && msr && Vect_MaxC){
+		vectC * vecset[3] = {Vect_MaxC, NULL, NULL};
+		int io = 0;
+		for (unsigned i = 0; i < msr->count; i++){
+			int needcalc = 1;
+			if (msr->point+i != Vect_MaxC){
+				vecset[1] = msr->point+i;
+				if (i < msr->count-1 && msr->point+i+1 != Vect_MaxC)
+					vecset[2] = msr->point+i+1;
+				else if (i < msr->count-2)
+					vecset[2] = msr->point+i+2;
+				else if (msr->point != Vect_MaxC)
+					vecset[2] = msr->point;
+				else if(msr->count>1) vecset[2] = msr->point+1;
+				else needcalc = 0;
+				if(needcalc){
+					if (io < CountMeasurePoints-1) calcOrient(vecset, orients+io);
+					io++;
+				}
+			}
+		}
+	}
 }
 
 int main(){
@@ -82,7 +151,10 @@ int main(){
 	init(&RealSource, &datameasure);
 	vectC * Vect_MaxC = findVectWithMaxConcentration(&datameasure);
 	if (Vect_MaxC){
-		print_Data(&RealSource, &datameasure, Vect_MaxC);
+		orient orients[CountMeasurePoints-1];
+		OrientInit(orients, &datameasure, Vect_MaxC);
+		//
+		print_Data(&RealSource, &datameasure, Vect_MaxC, orients);
 	}
 	return 0;
 }
